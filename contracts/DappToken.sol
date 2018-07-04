34b60b121a233cf07456c67333493bf9a952604e
Micheal Giles <mikegg21@gmail.com>
2018-07-04 15:33:57 -0500

    transfer tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -3,9 +3,32 @@ pragma solidity ^0.4.2;
 contract DappToken {
 
   uint256 public totalSupply;
+  string public name = 'micheal';
+  string public symbol = 'mgg';
+  string public standard = 'micheal Token v1.0';
 
-  constructor() public {
-      totalSupply = 1000000;
+  event Transfer(
+      address indexed _from,
+      address indexed _to,
+      uint256 _value
+  );
+
+  mapping(address => uint256) public balanceOf;
+
+  constructor(uint256 _initialSupply) public {
+      balanceOf[msg.sender] = _initialSupply;
+      totalSupply = _initialSupply;
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+      require(balanceOf[msg.sender] >= _value);
+
+      balanceOf[msg.sender] -= _value;
+      balanceOf[_to] += _value;
+
+      emit Transfer(msg.sender, _to, _value);
+
+      return true;
   }
 
 }
