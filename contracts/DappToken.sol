b0bff85e51aa252369e42f8f13c01dfa1c04cef5
Micheal Giles <mikegg21@gmail.com>
2018-07-05 12:32:12 -0500

    deligated transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -12,8 +12,14 @@ contract DappToken {
       address indexed _to,
       uint256 _value
   );
+  event Approval(
+      address indexed _owner,
+      address indexed _spender,
+      uint256 _value
+  );
 
   mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
 
   constructor(uint256 _initialSupply) public {
       balanceOf[msg.sender] = _initialSupply;
@@ -27,6 +33,29 @@ contract DappToken {
       balanceOf[_to] += _value;
 
       emit Transfer(msg.sender, _to, _value);
+
+      return true;
+  }
+
+  // Deligated Transfer
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+      allowance[msg.sender][_spender] = _value;
+
+      emit Approval(msg.sender, _spender, _value);
+
+      return true;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+      require(_value <= balanceOf[_from]);
+      require(_value <= allowance[_from][msg.sender]);
+
+      balanceOf[_from] -= _value;
+      balanceOf[_to] += _value;
+
+      allowance[_from][msg.sender] -= _value;
+
+      emit Transfer(_from, _to, _value);
 
       return true;
   }
